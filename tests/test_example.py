f44048ca29a5696ff1dbb4815eebab2dd54fd513
GiantCherry <lunev1981@mail.ru>
2025-03-22 21:27:05 +0300

    Auto tests: Test Data

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from hexlet_pytest.example import reverse
 
 def test_reverse():
@@ -6,3 +7,18 @@ def test_reverse():
 
 def test_reverse_for_empty_string():
     assert reverse('') == ''
+
+
+
+def get_test_data_path(filename):
+    return Path(__file__).parent / 'test_data' / filename
+
+def read_file(filename):
+    return get_test_data_path(filename).read_text()
+
+def test_reverse_long():
+    before_html = read_file('reverse.txt')
+    expected = read_file('result_reverse.txt')
+    actual = reverse(before_html)
+
+    assert actual == expected
